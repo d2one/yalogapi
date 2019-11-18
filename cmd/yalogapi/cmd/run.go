2d613ae0505311e187773054b178444d470d09f3
Kuznetsov Daniil <d.kuznetsov@office.ngs.ru>
2019-11-18 23:11:27 +0700

    Migrate to cobra

---
+++ cmd/yalogapi/cmd/run.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/d2one/yalogapi/internal/yalogapi"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: runCommand,
+}
+
+var conf *yalogapi.Config
+
+func runCommand(cmd *cobra.Command, args []string) {
+	fmt.Println(strings.Join(args, " "))
+	fmt.Println(conf)
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	runCmd.Flags().StringVarP(&cfgFile, "config", "c", "", "AWS region (required)")
+	runCmd.MarkFlagRequired("config")
+
+	runCmd.PersistentFlags().StringP("mode", "m", "", "available values [history, regular, regular_early]")
+	viper.BindPFlag("mode", runCmd.PersistentFlags().Lookup("mode"))
+
+	runCmd.PersistentFlags().String("startDate", "", "startDate")
+	viper.BindPFlag("startDate", runCmd.PersistentFlags().Lookup("startDate"))
+
+	runCmd.PersistentFlags().String("endDate", "", "endDate")
+	viper.BindPFlag("endDate", runCmd.PersistentFlags().Lookup("endDate"))
+
+	runCmd.PersistentFlags().StringP("source", "s", "", "available values [hits, visits]")
+	viper.BindPFlag("source", runCmd.PersistentFlags().Lookup("source"))
+
+	rootCmd.AddCommand(runCmd)
+
+}
+
+func initConfig() {
+	if cfgFile == "" {
+		return
+	}
+
+	viper.SetConfigFile(cfgFile)
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Using config file:", viper.ConfigFileUsed())
+	if err := viper.Unmarshal(&conf); err != nil {
+		fmt.Printf("unable to decode into config struct, %v", err)
+		os.Exit(1)
+	}
+	if err := conf.Validate(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- cmd/yalogapi/main.go
+++ cmd/yalogapi/main.go
@@ -1,91 +1,22 @@
+/*
+Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/d2one/yalogapi/internal/yalogapi"
-	"github.com/spf13/viper"
-	"github.com/urfave/cli/v2"
-)
+import "github.com/d2one/yalogapi/cmd/yalogapi/cmd"
 
 func main() {
-	var config string
-	var startDate string
-	var endDate string
-	var mode string
-	var source string
-
-	app := &cli.App{
-		Name:  "yalogapi",
-		Usage: "fight the loneliness!",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Required:    true,
-				Aliases:     []string{"c"},
-				Name:        "config",
-				Usage:       "config",
-				Destination: &config,
-			},
-			&cli.StringFlag{
-				Aliases:     []string{"sd"},
-				Name:        "startDate",
-				Usage:       "startDate",
-				Destination: &startDate,
-			},
-			&cli.StringFlag{
-				Aliases:     []string{"ed"},
-				Name:        "endDate",
-				Usage:       "endDate",
-				Destination: &endDate,
-			},
-			&cli.StringFlag{
-				Required:    true,
-				Aliases:     []string{"m"},
-				Name:        "mode",
-				Usage:       "mode",
-				Destination: &mode,
-			},
-			&cli.StringFlag{
-				Required:    true,
-				Aliases:     []string{"s"},
-				Name:        "source",
-				Usage:       "source",
-				Destination: &source,
-			},
-		},
-		Action: func(c *cli.Context) error {
-			var err error
-			viper.SetConfigType("yaml")
-			viper.SetConfigFile(config)
-			fmt.Println(config)
-			// read in config file and check your errors
-			if err := viper.ReadInConfig(); err != nil {
-				fmt.Println(err)
-			}
-			conf := &yalogapi.Config{
-				StartDate: startDate,
-				EndDate:   endDate,
-				Mode:      mode,
-				Source:    source,
-			}
-			err = viper.Unmarshal(conf)
-			if err != nil {
-				fmt.Printf("unable to decode into config struct, %v", err)
-			}
-			err = conf.Validate()
-			if err != nil {
-				fmt.Println(err)
-			}
-			yalogapi := yalogapi.NewYaLogApi(conf)
-			yalogapi.Run()
-			return nil
-		},
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
+  cmd.Execute()
 }
