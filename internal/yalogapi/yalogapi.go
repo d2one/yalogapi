c25bd18345af8397184e829d7eb3d9b295ebd5f7
Nadezhda Batyuchenko <n.batyuchenko@office.ngs.ru>
2019-11-26 21:31:18 +0700

    split requests

--- internal/yalogapi/yalogapi.go
+++ internal/yalogapi/yalogapi.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -34,7 +35,7 @@ type EvaluateResponse struct {
 
 const Host string = "https://api-metrika.yandex.ru"
 
-func NewUserRequest(config *Config) *UserRequest {
+func NewUserRequest(config *Config) UserRequest {
 	var fields []string
 	switch config.Source {
 	case "hits":
@@ -44,7 +45,7 @@ func NewUserRequest(config *Config) *UserRequest {
 		fields = config.Logsapi.VisitsField
 		break
 	}
-	return &UserRequest{
+	return UserRequest{
 		Token:     config.Logsapi.Token,
 		CounterID: config.Logsapi.CounterID,
 		StartDate: config.StartDate,
@@ -71,7 +72,7 @@ func (yalogapi *YaLogApi) Run() {
 }
 
 // getEvaluation returns estimation of Logs API
-func getEvaluation(userRequest *UserRequest) (LogRequestEvaluation, error) {
+func getEvaluation(userRequest UserRequest) (LogRequestEvaluation, error) {
 	uri := fmt.Sprintf(
 		"%s/management/v1/counter/%s/logrequests/evaluate?",
 		Host,
@@ -92,7 +93,7 @@ func getEvaluation(userRequest *UserRequest) (LogRequestEvaluation, error) {
 	return logRequest.LogRequestEvaluation, nil
 }
 
-func doGetRequest(uri string, userRequest *UserRequest) ([]byte, error) {
+func doGetRequest(uri string, userRequest UserRequest) ([]byte, error) {
 	request, err := http.NewRequest("GET", uri, nil)
 
 	errorMessage := fmt.Sprintf("Yandex api request failed. Uri %s", uri)
@@ -137,7 +138,7 @@ func doGetRequest(uri string, userRequest *UserRequest) ([]byte, error) {
 	return body, nil
 }
 
-func getAPIRequests(request *UserRequest) ([]UserRequest, error) {
+func getAPIRequests(request UserRequest) ([]UserRequest, error) {
 	evaluate, err := getEvaluation(request)
 	if err != nil {
 		return nil, err
@@ -149,19 +150,64 @@ func getAPIRequests(request *UserRequest) ([]UserRequest, error) {
 
 	requests := []UserRequest{}
 	if evaluate.Possible {
-		config := UserRequest{
-			Token:     request.Token,
-			CounterID: request.CounterID,
-			StartDate: request.StartDate,
-			EndDate:   request.EndDate,
-			Source:    request.Source,
-			Fields:    request.Fields,
-		}
-		requests = append(requests, config)
+		requests = append(requests, request)
 		return requests, nil
 	}
 
-	// build new request here
+	days, err := getDays(request)
+	fmt.Println(days)
+	fmt.Println(err)
+	if err != nil {
+		return nil, err
+	}
+
+	requestsNumber := int(days/evaluate.MaxPossibleDayQuantity) + 1
+	daysInPeriod := int(days/requestsNumber) + 1
+
+	startDate := request.StartDate
+	endDate := request.EndDate
+	for i := 0; i < requestsNumber; i++ {
+		startDate, endDate := getNewDates(startDate, endDate, daysInPeriod, i)
+		request.StartDate = startDate
+		request.EndDate = endDate
+
+		// @TODO mark every request (append a new struct with request body and request status)
+		requests = append(requests, request)
+	}
 
 	return requests, nil
+}
+
+// @TODO don`t validate date here
+func getDays(request UserRequest) (int, error) {
+	from := request.StartDate
+	dateFrom, err := time.Parse("2006-01-02", from)
+	if err != nil {
+		return 0, errors.New("Can not parse start date")
+	}
+
+	to := request.EndDate
+	dateTo, err := time.Parse("2006-01-02", to)
+	if err != nil {
+		return 0, errors.New("Can not parse end date")
+	}
+
+	days := int(dateTo.Sub(dateFrom).Hours() / 24)
+
+	return days, nil
+}
+
+// @TODO don`t validate date here
+func getNewDates(from string, to string, daysInPeriod int, partNumber int) (string, string) {
+	dateFrom, _ := time.Parse("2006-01-02", from)
+	newDateFrom := dateFrom.AddDate(0, 0, partNumber*daysInPeriod)
+
+	dateTo, _ := time.Parse("2006-01-02", to)
+	newDateTo := dateFrom.AddDate(0, 0, (partNumber+1)*daysInPeriod-1)
+
+	if newDateTo.After(dateTo) {
+		newDateTo = dateTo
+	}
+
+	return newDateFrom.Format("2006-01-02"), newDateTo.Format("2006-01-02")
 }
